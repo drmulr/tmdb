2d49a03b626f6eb9e2fc80bdea699812f91a9d90
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2018-07-30 11:46:11 -0400

    ESLint

--- src/App.js
+++ src/App.js
@@ -4,29 +4,35 @@ import './App.css';
 
 import Movie from './Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars',
-    desc: 'Awesome'
-  },
-  {
-    id: 2, 
-    title: 'The Matrix'
-  }
-];
-
-
-
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=dedc57357ee4715c48f64dd5ca30c8f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+      const movies = await res.json();
+
+      this.setState({
+        movies: movies.results
+      })
+
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   render() {
+    console.log(this.state.movies);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} /> )}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
       </div>
     );
   }
