a56db8ad38d25b39e549761275a4e9b55148d53b
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2018-07-30 14:28:02 -0400

    Movies list loading w/ detail. Implement router.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch,
+  Link,
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './Movie';
+import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
 
-class App extends Component {
-
-  state = {
-    movies: []
-  }
-
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=dedc57357ee4715c48f64dd5ca30c8f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
-      const movies = await res.json();
-
-      this.setState({
-        movies: movies.results
-      })
-
-    } catch(e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    console.log(this.state.movies);
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to Mull Movies</h1>
-          </header>
-
-          <Route path="/test" component={Test} />
-
-          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+        <h1 className="App-title">Welcome to Mull Movies</h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default App;
 
-const Test = () => (
-  <h1>Test</h1>
+const Test = ({ match }) => (
+  <h1>{match.params.id}</h1>
 )
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = props => (
     <div>
-        <img src={`${POSTER_PATH}${props.movie.poster_path}`} alt={props.movie.title} />
+        <Link to={`/${props.movie.id}`}>
+            <img src={`${POSTER_PATH}${props.movie.poster_path}`} alt={props.movie.title} />
+        </Link>
     </div>  
 );
 
