6afd627c4016e993af6f8fddbdfd278c0c320dec
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2018-07-30 10:16:01 -0400

    Iterating over test movies, first component created.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
 const movies = [
   {
     id: 1,
@@ -23,13 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {movies.map((movie) => {
-          return (
-            <div>
-              {movie.title}
-            </div>
-          )
-        })}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
       </div>
     );
   }
